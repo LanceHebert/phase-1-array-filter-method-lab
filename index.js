29fbe32427fa192f4dc6a6d05f337f9c1c123ff5
Lance Hebert <LSUHEBERT@gmail.com>
2021-11-26 20:45:01 -0800

    commit tree 1ffcba3b0d1c9e22a6b1e8b076f9ed578aa5b3ed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+function findMatching(array,stringPassed)
+{
+
+    const filterMatch = array.filter(word =>
+    {
+        return word.toLowerCase() === stringPassed.toLowerCase()                
+    });
+    return filterMatch;
+}
+
+function fuzzyMatch(array,names)
+{
+    const nameDigit = names.length;
+
+    const filterMatch = array.filter(word => 
+    {
+        return word.slice(0,nameDigit) === names                
+    })
+    return filterMatch;
+}
+
+function matchName(array , names)
+{
+    const filterMatch = array.filter(arrayObject => 
+    {
+        return  arrayObject.name === names       
+    })
+    return filterMatch;
+}
